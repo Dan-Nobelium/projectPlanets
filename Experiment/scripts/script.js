83319187f7ce09a934c9c05bbaeb626821d2dc54
Dan-Nobelium <Daniel.nobelium@gmail.com>
2023-11-27 23:00:48 +1100

    not running

--- Experiment/scripts/script.js
+++ Experiment/scripts/script.js
@@ -1,6 +1,8 @@
 
+console.log("Script started");
+
 // Import functions from psychometrics.js
-import { loadQuestionnaire, createQuestionBlock } from './psychometrics.js';
+//import { loadQuestionnaire, createQuestionBlock } from './psychometrics.js';
 
 
 /* Experiment Parameters */
@@ -55,6 +57,51 @@ var images = [
   //----------------------------------------------------------------------------
   /* instructions text */
 
+const consent_text = [
+    '<p>Before you begin, please read the information sheet carefully (you can download the pdf <a href="https://jessicaleephd.files.wordpress.com/2020/07/pis_sona_3385.pdf" target="_blank">here</a>)</p>' +
+    '<br>' +
+    '<p align="center"><b>PARTICIPANT CONSENT</b></p>' +
+    '<center><img src="./img/PIS_1.jpg"></center>' +
+    '<center><img src="./img/PIS_2.jpg"></center>' +
+    '<center><img src="./img/consent.jpg"></center>' +
+    'By continuing, you are making a decision whether or not to participate. <br>  Clicking the button below indicates that, having read the information provided on the participant information sheet, you consent to the above.' +
+    '<br></p>'
+  ];
+
+  const demo_text = [
+    '<p> Gender: ' +
+    '<input type="radio" name="gender" value="male" required/> Male &nbsp; ' +
+    '<input type="radio" name="gender" value="female" required/> Female &nbsp;' +
+    '<input type="radio" name="gender" value="other" required/> Other<br>' + '<br>' +
+    '<p> Age: <input name="age" type="text" required/> </p>' + '<br>' +
+    '<p> Native language: <input name="language" type="text" required/> </p>' + '<br>'
+  ];
+
+  if (sample === "MTurk") {
+    var MTurk_insert = [
+      '<p>If anything goes wrong during the experiment, please take a screenshot and notify the requester. Do <b>not</b> press the BACK button or quit out of the program. This will make it hard for you to get paid.</p>' +
+      '<p>If you complete the task, you will get your payment no matter what. Please take your time and think about your predictions and judgements seriously. </p>'
+    ]
+  } else {
+    var MTurk_insert = '';
+  }
+
+  var ins = {};
+
+  ins.pretrain1 = [
+    '<p>WELCOME TO THE EXPERIMENT! </p>' +
+    '<p>Throughout the experiment, please read all instructions <b>carefully</b> and click on the buttons to go forward or back. You may need to scroll down on some pages. </p>' +
+    MTurk_insert +
+    '<p>Please <b>do not</b> hit refresh or the back button on your browser as you can only do the experiment ONCE.</p>' +
+    '<p>Please complete the experiment in ONE sitting in FULL SCREEN mode.</p>'
+  ];
+
+  ins.pretrain2 = [
+    '<p>In this experiment you will be playing a game over 6 blocks. </p>' +
+    '<p>In this game, you are an intergalactic trader in space. You will be situated between two planets that you can trade with. You can send a signal to each planet by clicking on them. Sometimes locals on these planets will receive the signal and be willing to trade. Each successful trade will give you points. </p>' +
+    '<p>Your goal is to have as many points as possible. </p>'
+    // '<p><b>Note:</b> Whatever you earn in-game will be converted into real money for you at the end of the experiment. The more you earn in-game, the more you make in real life. You can earn more points by trading with both planets. </p>'
+  ];
 
   ins.pretrain3 = [
     '<p>You can click on each of the planets as many times as you like. Just remember, the aim is to get as many points as possible! </p>' +
@@ -290,15 +337,6 @@ var images = [
   var timeline = [];
   var block6loop = []
 
-// Load and create questionnaire blocks dynamically
-async function setupQuestionnaires() {
-    const questionnaires = ['dass', 'bis', 'aor', 'bisbas', 'ipip'];
-    for (let q of questionnaires) {
-        const data = await loadQuestionnaire(`psychometrics/${q}_survey.json`);
-        const block = createQuestionBlock(data);
-        timeline.push(block);
-    }
-}
 
 
 
@@ -842,7 +880,16 @@ timeline.push(demographics_block);
         nBlocks_p2: nBlocks_p2
     });
 
-      
+// Load and create questionnaire blocks dynamically
+async function setupQuestionnaires() {
+  const questionnaires = ['dass', 'bis', 'aor', 'bisbas', 'ipip'];
+  for (let q of questionnaires) {
+      const data = await loadQuestionnaire(`psychometrics/${q}_survey.json`);
+      const block = createQuestionBlock(data);
+      timeline.push(block);
+  }
+}
+
 // Call setupQuestionnaires and then initialize the experiment
 setupQuestionnaires().then(() => {
   // Initialize jsPsych
